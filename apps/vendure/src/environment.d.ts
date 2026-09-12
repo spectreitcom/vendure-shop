5bdc48b2165b867d8185b7ebfc34711a74b60466
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-09-12 10:45:22 +0200

    chore: add required apps, tests and ci workflow

---
+++ apps/vendure/src/environment.d.ts
@@ -0,0 +1,22 @@
+export {};
+
+// Here we declare the members of the process.env object, so that we
+// can use them in our application code in a type-safe manner.
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      APP_ENV: string;
+      VENDURE_SERVER_PORT: string;
+      PORT: string;
+      COOKIE_SECRET: string;
+      SUPERADMIN_USERNAME: string;
+      SUPERADMIN_PASSWORD: string;
+      CORS_ORIGINS?: string;
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_NAME: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+    }
+  }
+}
---
+++ apps/vendure/src/sample.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("vendure app test", () => {
+  it("basic test passes", () => {
+    expect(true).toBe(true);
+  });
+});
