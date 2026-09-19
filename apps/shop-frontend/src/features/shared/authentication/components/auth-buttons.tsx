3a3591d4825422d259ec34ba08d74bf10668e667
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-09-19 15:03:57 +0200

    chore: refresh cart state on user logout

--- apps/shop-frontend/src/features/shared/authentication/components/auth-buttons.tsx
+++ apps/shop-frontend/src/features/shared/authentication/components/auth-buttons.tsx
@@ -2,16 +2,23 @@ import { Link, useRouter } from '@tanstack/react-router';
 import { Button } from '@mui/material';
 import { logout, useActiveUser } from '#/features/shared/authentication';
 import { useServerFn } from '@tanstack/react-start';
+import { useActiveCart } from '#/features/shared/cart';
 
 export function AuthButtons() {
-  const { isFetching, activeUser, refresh } = useActiveUser();
+  const {
+    isFetching,
+    activeUser,
+    refresh: refreshActiveUser,
+  } = useActiveUser();
+  const { refresh: refreshActiveCart } = useActiveCart();
   const router = useRouter();
 
   const logoutFn = useServerFn(logout);
 
   const handleLogout = async () => {
     await logoutFn();
-    await refresh();
+    await refreshActiveUser();
+    await refreshActiveCart();
     await router.navigate({ to: '/auth/login' });
   };
 
