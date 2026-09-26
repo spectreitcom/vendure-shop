f2ff2b27947e753da52fa6357048d41e0aed7da8
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-09-26 13:41:32 +0200

    chore: add ESLint configuration with TypeScript support, integrate Prettier, and update package.json with lint scripts and dependencies

---
+++ apps/vendure/eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "static/**",
+      ".vendure/**",
+      ".tanstack/**",
+      "src/gql/**",
+      "src/migrations/**",
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  prettierConfig,
+);
